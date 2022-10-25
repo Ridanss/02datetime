b1d6c697f5b54ed5f3f8b5dfeabed36e96c2c116
Ridanss <pavlonk.04@spst.eu>
2022-10-25 11:10:32 +0200

    p02

---
+++ p02/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p02
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            DateTime birth = dateTimePicker1.Value;
+            TimeSpan age = dt - birth;
+            labelAge.Text = String.Format("Jsi starý {0}", age.ToString("f"));
+        }
+    }
+}
--- p02/Program2.cs
+++ p02/Program2.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace p02
 {
-    internal static class Program
+    internal static class Program2
     {
         /// <summary>
         /// Hlavní vstupní bod aplikace.
@@ -16,7 +16,7 @@ namespace p02
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
