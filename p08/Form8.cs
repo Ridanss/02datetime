c540f1cf19e743834ab4720dbbb3a109e24be8c5
Ridanss <pavlonk.04@spst.eu>
2022-11-08 10:59:57 +0100

    p08

---
+++ p08/Form8.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p08
+{
+    public partial class Form8 : Form
+    {
+        public Form8()
+        {
+            InitializeComponent();
+        }
+
+        public static bool ZarukaRoky(DateTime bought, int years, out int timeleft)
+        {
+            DateTime today = DateTime.Today;
+
+            TimeSpan left = today - (bought.AddYears(years));
+            timeleft = left.Days;
+            if (timeleft < 0) return true;
+            return false;
+
+        }
+        private void buttonExecute_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text,out int let))
+            {
+                labelInsurance.Text = "záruka: ";
+                DateTime bought = dateTimePicker1.Value;
+                bool zaruka = ZarukaRoky(bought, let, out int zbyva);
+                if (zaruka) labelInsurance.Text += "Zbývá " + Math.Abs(zbyva) + " dní";
+                else labelInsurance.Text += "Vypršela";
+            }
+
+        }
+    }
+}
